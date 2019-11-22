85ef980bfd3cd6142cbf827d1d6eb56f5e229221
ratrevorrow <ratrevorrow@my.waketech.edu>
2019-11-22 15:32:56 -0500

    Added search filter (incomplete)

--- saucey-saucer/src/components/HorizontalScroll/BeerScroll.js
+++ saucey-saucer/src/components/HorizontalScroll/BeerScroll.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import "./horizontal-scroll.css";
-import { Card } from "react-bootstrap";
-import MyVerticallyCenteredModal from "../Modal/MyVerticallyCenteredModal";
+import React, { useState, useEffect } from 'react';
+import './horizontal-scroll.css';
+import { Card } from 'react-bootstrap';
+import MyVerticallyCenteredModal from '../Modal/MyVerticallyCenteredModal';
 
 function BeerScroll(props) {
   const [modalShow, setModalShow] = React.useState(false);
   const [beers, setBeers] = React.useState(null);
-  const [style, setStyle] = React.useState("");
-
+  const [style, setStyle] = React.useState('');
+  
   useEffect(() => {
     setStyle(props.beerStyle.style);
     getBeerList(props.beerStyle.beers);
@@ -24,9 +24,7 @@ function BeerScroll(props) {
         >
           <Card.Body>
             <Card.Title>{beer.brewery}</Card.Title>
-            <Card.Text>
-              {beer.name}
-            </Card.Text>
+            <Card.Text>{beer.name}</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Checked in 3 mins ago</small>
@@ -38,8 +36,8 @@ function BeerScroll(props) {
 
   return (
     <div>
-      <hr style={{ border: "2px solid green" }} />
-      <h3 style={{ color: "white" }}>{style}</h3>
+      <hr style={{ border: '2px solid green' }} />
+      <h3 style={{ color: 'white' }}>{style}</h3>
       <div className="scrolling-wrapper-flexbox">{beers}</div>
     </div>
   );
--- saucey-saucer/src/components/SearchBeers/SearchBeers.js
+++ saucey-saucer/src/components/SearchBeers/SearchBeers.js
@@ -1,24 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Card } from "react-bootstrap";
-import HorizontalScroll from "../HorizontalScroll/HorizontalScroll";
-import BeerScroll from "../HorizontalScroll/BeerScroll";
-import MyVerticallyCenteredModal from "../Modal/MyVerticallyCenteredModal";
+import React, { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+import HorizontalScroll from '../HorizontalScroll/HorizontalScroll';
+import BeerScroll from '../HorizontalScroll/BeerScroll';
+import MyVerticallyCenteredModal from '../Modal/MyVerticallyCenteredModal';
+import { Input } from 'antd';
 
 function SearchBeers(props) {
   const [modalShow, setModalShow] = React.useState(false);
   const [allBeers, setAllBeers] = React.useState(null);
+  const [filteredBeers, setFilteredBeers] = React.useState(null);
 
   useEffect(() => {
-    getAllBeers(props.allBeers);
+    setAllBeers(props.allBeers);
   }, []);
 
-  const getAllBeers = allBeers => {
-    setAllBeers(
-      allBeers.map((style, idx) => <BeerScroll beerStyle={style} key={idx} />)
+  const getAllBeers = (filter) => {
+    const beersFiltered = allBeers.filter(obj => {
+      return obj.style.toLowerCase().includes(filter);
+    });
+    
+    setFilteredBeers(
+      beersFiltered.map((style, idx) => <BeerScroll beerStyle={style} key={idx} />)
     );
+    // console.log(filter);
+    console.log(filteredBeers);
+  };
+  
+  const filterByName = event => {
+    const userText = event.target.value;
+    if (userText)
+      getAllBeers(userText);
+    else
+      getAllBeers('');
   };
 
-  return <div>{allBeers}</div>;
+  return (
+    <div>
+      {props.allBeers && <Input onChange={filterByName} type="search" />}
+      {filteredBeers}
+    </div>
+  );
 }
 
 export default SearchBeers;
